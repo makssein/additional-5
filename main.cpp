87afde1438862a33a6d140f8835d943fd04c8984
Maxim <90086818+makssein@users.noreply.github.com>
2021-10-11 19:03:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include "func.h"
 #define N 10000
-#define M 20000
 
 int main(){
-	int n, a, sum, b;
-	int mas[N];
-	sum = 0;
-	int c = 0;
-	int num = 0;
-	int k = 0;
+    int n, s;
+    s=0;
+    int mas[N];
+    int count[N];
+    int c = 0;
 
-	std::ifstream in("input.txt");
-	std::ofstream out("output.txt");
-	
-	in >> n;
-	for (int i = 0; i < n-1; i++) {
-		in >> mas[i];
-	}
-	for (int i = 0; i < n-1; i++) {
-		a = mas[i];
-		while (a > 0) {
-			b = a % 10;
-			sum += b;
-			a /= 10;
-		}
-		if (!(sum % 6 == 0)) {
-			mas[c] = mas[i];
-			c += 1;
-		}
-		b = 0;
-		sum = 0;
-
-	}
-	for (int i = 0; i < c; i++) {
-		num = mas[i];
-		while (num > 9)
-			num = num / 10;
-		//if (num == 1) {
-		//}
-	}
-	for (int i = 0; i < c; k++) {
-		out << mas[i] << " ";
-	}
-
-	
-
+    func::Input(n, mas);
+    func::multiplicity6(n, mas, c);
+    func::duplication(c, s, count, mas);
+    func::Output(c, s, mas);
 
 }
-
